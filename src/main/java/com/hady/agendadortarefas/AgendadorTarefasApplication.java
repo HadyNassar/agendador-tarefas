29aa3286fddbb3871d416acb09ea62303312ef5f
HadyNassar <Hadynassar02@gmail.com>
2026-02-15 18:54:55 -0400

    commit inicial com esqueleto

---
+++ src/main/java/com/hady/agendadortarefas/AgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.hady.agendadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTarefasApplication.class, args);
+	}
+
+}
